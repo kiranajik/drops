007289480ab97696ee5fd1f4c5670c6b177f3b2b
kiranajik <kiranlcw31.anair@gmail.com>
2022-12-13 21:34:01 +0530

    for midhun

--- app/src/main/java/co/kirel/drops/splash.java
+++ app/src/main/java/co/kirel/drops/splash.java
@@ -2,6 +2,9 @@ package co.kirel.drops;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.view.View;
 import android.os.Bundle;
 
 public class splash extends AppCompatActivity {
@@ -10,5 +13,18 @@ public class splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(splash.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 4000);
     }
 }
