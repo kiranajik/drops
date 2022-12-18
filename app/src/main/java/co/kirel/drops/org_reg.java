bbff90f4de14f7a590b13c45371e1de0b7093c38
Kiran Ajikumar <73101486+kiranajik@users.noreply.github.com>
2022-12-18 23:24:14 +0530

    3nd

---
+++ app/src/main/java/co/kirel/drops/org_reg.java
@@ -0,0 +1,14 @@
+package co.kirel.drops;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class org_reg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_org_reg);
+    }
+}
