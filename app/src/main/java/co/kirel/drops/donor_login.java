e1a595c64509134c9515dfb6522976eaa0766a53
Kiran Ajikumar <73101486+kiranajik@users.noreply.github.com>
2022-12-20 11:03:00 +0530

    push2

--- app/src/main/java/co/kirel/drops/donor_login.java
+++ app/src/main/java/co/kirel/drops/donor_login.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -62,6 +63,7 @@ public class donor_login extends AppCompatActivity {
                             @Override
                             public void onFailure(@NonNull Exception e) {
                                 Toast.makeText(donor_login.this, "Failed Login", Toast.LENGTH_SHORT).show();
+                                Log.d("login",e.getMessage());
                             }
                         });
             }
