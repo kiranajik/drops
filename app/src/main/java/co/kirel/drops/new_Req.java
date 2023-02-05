1b6b766ca847f778cdc7eb11a821aadc5f1ba8e2
midhun <midhunmurali776@gmail.com>
2023-02-05 14:00:52 +0530

    Hospital home with bottom nav added

---
+++ app/src/main/java/co/kirel/drops/new_Req.java
@@ -0,0 +1,14 @@
+package co.kirel.drops;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class new_Req extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_req);
+    }
+}
